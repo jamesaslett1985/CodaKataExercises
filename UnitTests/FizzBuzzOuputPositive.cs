017709d5660056ae1631e002d6567d1f2861d4c9
jamesaslett1985 <jamesaslett1985@gmail.com>
2020-09-15 21:10:07 +0100

    Tweaks

--- UnitTests/FizzBuzzOuputPositive.cs
+++ UnitTests/FizzBuzzOuputPositive.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System.Linq;
 using Moq;
+using System.ComponentModel.DataAnnotations;
 
 namespace CodeKata.UnitTests
 {
@@ -12,6 +13,10 @@ namespace CodeKata.UnitTests
         [TestCase]
         public void FizzBuzzOutput()
         {
+            //need to run through 1-00 and assert outputs. What's the best way to create the expected results? Presumably not newing up FizzBuzzOutput as that 
+            //would be pointless. Mock?
+
+            var fizzBuzz = new FizzBuzzOutput();
             var getNumbers = new NumberOnlyOutput();
             var processNumbers = new OneToOneHundredProcessor<int>(getNumbers);
             var results = processNumbers.LoopThroughNumbers().Select(item => item.ToString());
